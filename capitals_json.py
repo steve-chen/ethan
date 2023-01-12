0b75e0b7f99c4f88db6f1836575ae5f5055c679a
steve-chen <sschen@me.com>
2023-01-12 22:26:13 +0800

    Correcting the way we iterate through the JSON dictionary

--- capitals_json.py
+++ capitals_json.py
@@ -1,26 +1,23 @@
 import json
 import sys
 
-
 with open("country-list.json", "r") as jsonFile:
-    capitals = json.load(jsonFile)
+    all_countries = json.load(jsonFile)
     jsonFile.close()
 
 if len(sys.argv) == 1:
-    for line in capitals:
-        country, capital = line.keys(), line.values()
-        print(f"The capital of {country} is {capital}.")
+    for country in all_countries:
+        country_name, capital_name = country['country'], country['capital']
+        print(f"The capital of {country_name} is {capital_name}.")
 elif len(sys.argv) == 2:
     ask_country = sys.argv[1]
-    print(f"Looking up the capital for {ask_country}")
+    print(f"Looking up the capital for {ask_country.capitalize()}")
 
-
-    for line in capitals:
-        country, capital = line.keys(), line.values()
-        if country.lower() == ask_country.lower():
-            print(f"The capital of {ask_country} is {capital}.")
+    for country in all_countries:
+        country_name, capital_name = country['country'], country['capital']
+        if country_name.lower() == ask_country.lower():
+            print(f"Found it! The capital of {ask_country.capitalize()} is {capital_name}.")
             sys.exit(0)
-    
     print(f"We couldn't find your country!")
 elif len(sys.argv) > 2:
     print("That capitals isn't on our list.")
